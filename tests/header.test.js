df57e810734894ffd4652b63a0a046d3aceb06f7
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-14 04:06:37 +0600

    Put tests in a new describe block

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,29 +1,31 @@
-const Page = require("./helpers/page")
+const Page = require("./interfaces/page")
 
 let page
 
 beforeEach(async () => {
   page = await Page.build()
   await page.goto("localhost:3000")
-}, 50000)
+})
 
 afterEach(async () => {
   await page.close()
 })
 
-test("header has the correct text", async () => {
-  const text = await page.$eval("a.brand-logo", el => el.innerHTML)
-  expect(text).toEqual("Blogster")
-})
+describe("App header", async () => {
+  test("has the correct text", async () => {
+    const text = await page.getContentsOf("a.brand-logo")
+    expect(text).toEqual("Blogster")
+  })
 
-test("clicking login starts oauth flow", async () => {
-  await page.click(".right a")
-  const url = page.url()
-  expect(url).toMatch(/accounts\.google\.com/)
-})
+  test("login-button click starts oauth flow", async () => {
+    await page.click(".right a")
+    const url = page.url()
+    expect(url).toMatch(/accounts\.google\.com/)
+  })
 
-test("show logout button when signed in", async () => {
-  await page.login()
-  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML)
-  expect(text).toEqual("Logout")
+  test("shows logout button when signed in", async () => {
+    await page.login()
+    const text = await page.getContentsOf('a[href="/auth/logout"]')
+    expect(text).toEqual("Logout")
+  })
 })
