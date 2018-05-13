0a207f6609b0abcf592b9299ef3b9a9a02d10715
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-13 17:20:35 +0600

    React client blog post app

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+import BlogList from "./blogs/BlogList"
+
+const Dashboard = () => (
+  <div>
+    <BlogList />
+    <div className="fixed-action-btn">
+      <Link to="/blogs/new" className="btn-floating btn-large red">
+        <i className="material-icons">add</i>
+      </Link>
+    </div>
+  </div>
+)
+
+export default Dashboard
---
+++ client/src/reducers/blogsReducer.js
@@ -0,0 +1,18 @@
+import mapKeys from "lodash/mapKeys"
+import { FETCH_BLOGS, FETCH_BLOG } from "../actions/types"
+
+export default function(state = {}, action) {
+  const { type, payload } = action
+
+  switch (type) {
+    case FETCH_BLOG:
+      const blog = payload
+      return { ...state, [blog._id]: payload }
+
+    case FETCH_BLOGS:
+      return { ...state, ...mapKeys(payload, "_id") }
+
+    default:
+      return state
+  }
+}
