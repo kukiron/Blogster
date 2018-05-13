bbd665e70fd32f185eb81697b9d72344e009e643
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-13 17:25:51 +0600

    Add test suite for blog app

---
+++ tests/header.test.js
@@ -0,0 +1,29 @@
+const Page = require("./helpers/page")
+
+let page
+
+beforeEach(async () => {
+  page = await Page.build()
+  await page.goto("localhost:3000")
+}, 50000)
+
+afterEach(async () => {
+  await page.close()
+})
+
+test("header has the correct text", async () => {
+  const text = await page.$eval("a.brand-logo", el => el.innerHTML)
+  expect(text).toEqual("Blogster")
+})
+
+test("clicking login starts oauth flow", async () => {
+  await page.click(".right a")
+  const url = page.url()
+  expect(url).toMatch(/accounts\.google\.com/)
+})
+
+test("show logout button when signed in", async () => {
+  await page.login()
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML)
+  expect(text).toEqual("Logout")
+})
