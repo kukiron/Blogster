e21d23e8a60ae5cb15838c8f9f605277a26dc87c
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-14 14:01:49 +0600

    Fix typo with monogdb port

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     "824990695575-16ih4q6a1rbnug21se0r5mkmb6urtg8e.apps.googleusercontent.com",
   googleClientSecret: "Eic6GHp4lTrpDGLM9FdvkPaW",
-  mongoURI: "monogdb://127.0.0.1:27017/blogster",
+  mongoURI: "mongodb://127.0.0.1:27017/blogster_nodeci",
   redisUrl: "redis://127.0.0.1:6379",
   cookieKey: "123123123"
 }
