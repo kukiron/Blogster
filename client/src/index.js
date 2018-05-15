b09c0513e68d336bd5ffb81d56b4eb234f1b50a6
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-15 16:12:35 +0600

    Impose eslint-prettier custom rules

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware } from "redux"
+import reduxThunk from "redux-thunk"
 
-import App from './components/App';
-import reducers from './reducers';
-import 'materialize-css/dist/css/materialize.min.css';
+import App from "./components/App"
+import reducers from "./reducers"
+import "materialize-css/dist/css/materialize.min.css"
 
-import axios from 'axios';
-window.axios = axios;
+import axios from "axios"
+window.axios = axios
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.querySelector('#root')
-);
+  document.querySelector("#root")
+)
--- client/src/reducers/blogsReducer.js
+++ client/src/reducers/blogsReducer.js
@@ -5,9 +5,10 @@ export default function(state = {}, action) {
   const { type, payload } = action
 
   switch (type) {
-    case FETCH_BLOG:
+    case FETCH_BLOG: {
       const blog = payload
       return { ...state, [blog._id]: payload }
+    }
 
     case FETCH_BLOGS:
       return { ...state, ...mapKeys(payload, "_id") }
