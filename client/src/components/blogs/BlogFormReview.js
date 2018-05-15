bd1511a9e76a9a9666151e8582699d1dea3085b3
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-15 15:17:36 +0600

    Option for adding image to the newly created blog post

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -1,27 +1,27 @@
 // BlogFormReview shows users their form inputs for review
-import _ from 'lodash';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import formFields from './formFields';
-import { withRouter } from 'react-router-dom';
-import * as actions from '../../actions';
+import _ from "lodash"
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import formFields from "./formFields"
+import { withRouter } from "react-router-dom"
+import * as actions from "../../actions"
 
 class BlogFormReview extends Component {
-  renderFields() {
-    const { formValues } = this.props;
+  state = { file: null }
 
-    return _.map(formFields, ({ name, label }) => {
-      return (
-        <div key={name}>
-          <label>{label}</label>
-          <div>{formValues[name]}</div>
-        </div>
-      );
-    });
+  renderFields = () => {
+    const { formValues } = this.props
+
+    return _.map(formFields, ({ name, label }) => (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    ))
   }
 
-  renderButtons() {
-    const { onCancel } = this.props;
+  renderButtons = () => {
+    const { onCancel } = this.props
 
     return (
       <div>
@@ -36,31 +36,38 @@ class BlogFormReview extends Component {
           <i className="material-icons right">email</i>
         </button>
       </div>
-    );
+    )
   }
 
-  onSubmit(event) {
-    event.preventDefault();
+  onFileChange = e => {
+    this.setState({ file: e.target.files[0] })
+  }
 
-    const { submitBlog, history, formValues } = this.props;
+  onSubmit = event => {
+    event.preventDefault()
 
-    submitBlog(formValues, history);
+    const { file } = this.state
+    const { submitBlog, history, formValues } = this.props
+    submitBlog(formValues, file, history)
   }
 
   render() {
     return (
-      <form onSubmit={this.onSubmit.bind(this)}>
+      <form onSubmit={this.onSubmit}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
 
+        <h5>Add an image</h5>
+        <input onChange={this.onFileChange} type="file" accept="image/*" />
+
         {this.renderButtons()}
       </form>
-    );
+    )
   }
 }
 
-function mapStateToProps(state) {
-  return { formValues: state.form.blogForm.values };
-}
+const mapStateToProps = ({ form: { blogForm } }) => ({
+  formValues: blogForm.values
+})
 
-export default connect(mapStateToProps, actions)(withRouter(BlogFormReview));
+export default connect(mapStateToProps, actions)(withRouter(BlogFormReview))
