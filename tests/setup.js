d2e7c8905f3d97a54f58a5e572678b1e4bdec2c9
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-15 15:21:27 +0600

    Increae timeout for Jest

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,4 @@
-jest.setTimeout(100000)
+jest.setTimeout(120000)
 
 require("../models/User")
 
@@ -6,4 +6,4 @@ const mongoose = require("mongoose")
 const keys = require("../config/keys")
 
 mongoose.Promise = global.Promise
-mongoose.connect(keys.mongoURI, { useMongoClient: true })
+mongoose.connect(keys.mongoURI)
