354c0c09ea068eec487e3b774e2df574a324ef69
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-15 16:13:35 +0600

    Remove config/dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    "824990695575-16ih4q6a1rbnug21se0r5mkmb6urtg8e.apps.googleusercontent.com",
-  googleClientSecret: "Eic6GHp4lTrpDGLM9FdvkPaW",
-  mongoURI: "mongodb://kukiron:blogpost@ds217560.mlab.com:17560/blog_dev",
-  redisUrl: "redis://127.0.0.1:6379",
-  cookieKey: "123123123"
-}
